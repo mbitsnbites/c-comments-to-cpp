8fa7529527d1e1b11af8d555d77746f018808261
m <m@bitsnbites.eu>
2020-11-30 08:24:28 +0100

    Apply flake8 and black

--- c-comments-to-cpp.py
+++ c-comments-to-cpp.py
@@ -20,16 +20,18 @@
   3. This notice may not be removed or altered from any source distribution.
 """
 
-import argparse, fileinput
+import argparse
+import fileinput
 
 # Handle the program arguments.
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawTextHelpFormatter,
-    description='Convert C-style comments to C++-style.')
+    description="Convert C-style comments to C++-style.",
+)
 args = parser.parse_args()
 
 inside_c_comment = False
-comment_style = '//'
+comment_style = "//"
 comment_indent = 0
 
 # Whether or not to include comments for start/end lines:
@@ -59,7 +61,7 @@ comment_empty_lines_in_comment_blocks = False
 for line in fileinput.input():
     # Start by dropping trailing whitespace from the input.
     line = line.rstrip()
-    out_line = ''
+    out_line = ""
 
     # Cater for empty lines in the middle of comments
     # which we want to be empty comment lines to make
@@ -73,64 +75,66 @@ for line in fileinput.input():
     k = 0
     while k < len(line):
         if inside_string:
-            assert(k > 0)
-            if line[k] == '"' and line[k - 1] != '\\':
+            assert k > 0
+            if line[k] == '"' and line[k - 1] != "\\":
                 inside_string = False
             out_line += line[k]
             k += 1
         else:
             if inside_c_comment:
                 if inside_indentation:
-                    if (k >= comment_indent) or not (line[k] in [' ', '\t']):
+                    if (k >= comment_indent) or not (line[k] in [" ", "\t"]):
                         inside_indentation = False
                         out_line += comment_style
                         # Consume up to len(comment_style) chars from the line.
                         for m in range(k, min(k + len(comment_style), len(line))):
-                            if line[m] in [' ', '\t']:
+                            if line[m] in [" ", "\t"]:
                                 k += 1
-                            elif line[m] == '*' and ((m + 1) == len(line) or line[m + 1] != '/'):
+                            elif line[m] == "*" and (
+                                (m + 1) == len(line) or line[m + 1] != "/"
+                            ):
                                 k += 1
                             else:
                                 break
-                        if k < len(line) and not (line[k] in [' ', '\t', '*']):
-                            out_line += ' '
+                        if k < len(line) and not (line[k] in [" ", "\t", "*"]):
+                            out_line += " "
                 if k < len(line):
-                    if line[k] == '*' and (k + 1) < len(line) and line[k + 1] == '/':
+                    if line[k] == "*" and (k + 1) < len(line) and line[k + 1] == "/":
                         inside_c_comment = False
                         start_or_end_line = True
-                        if k > 0 and line[k - 1] == '*':
+                        if k > 0 and line[k - 1] == "*":
                             # Replace '*/' with '**' in case this is a '...*****/'-style line.
-                            out_line += '**'
-                        out_line += '\n'
+                            out_line += "**"
+                        out_line += "\n"
                         k += 2
                     else:
                         out_line += line[k]
                         k += 1
             else:
-                if line[k] == '/' and (k + 1) < len(line) and line[k + 1] == '*':
+                if line[k] == "/" and (k + 1) < len(line) and line[k + 1] == "*":
                     # Start of C style comment.
                     comment_indent = k
                     inside_c_comment = True
                     start_or_end_line = True
-                    comment_style = '//'
+                    comment_style = "//"
                     k += 2
-                    if (k + 1) < len(line) and line[k] == '*' and line[k+1] == '/':
+                    if (k + 1) < len(line) and line[k] == "*" and line[k + 1] == "/":
                         # Ridiculous case of a completely empty, single line C-style comment
                         inside_c_comment = False
                         start_or_end_line = True
-                        break;
-                    if k < len(line) and (line[k] == '*' or line[k] == '!'):
-                        if (k + 1) >= len(line) or line[k + 1] != '*':
+                        break
+                    if k < len(line) and (line[k] == "*" or line[k] == "!"):
+                        if (k + 1) >= len(line) or line[k + 1] != "*":
                             # Start of Doxygen comment.
-                            comment_style = '///'
+                            comment_style = "///"
                             k += 1
-                            if k < len(line) and line[k] == '<':
+                            if k < len(line) and line[k] == "<":
                                 # Start of Doxygen after-member comment.
-                                comment_style = '///<'
+                                comment_style = "///<"
                                 k += 1
                     out_line += comment_style
                     inside_indentation = False
-                elif line[k] == '/' and (k + 1) < len(line) and line[k + 1] == '/':
+                elif line[k] == "/" and (k + 1) < len(line) and line[k + 1] == "/":
                     # Start of C++ style comment.
                     out_line += line[k:]
                     k = len(line)
@@ -144,6 +148,8 @@ for line in fileinput.input():
     out_line = out_line.rstrip()
 
     # Print output if options and circumstances permit.
-    if (not start_or_end_line or comment_start_or_end_lines) and (len(out_line.lstrip()) != len(comment_style) or comment_empty_lines_in_comment_blocks):
-        print('%s\n' % (out_line), end=' ')
-
+    if (not start_or_end_line or comment_start_or_end_lines) and (
+        len(out_line.lstrip()) != len(comment_style)
+        or comment_empty_lines_in_comment_blocks
+    ):
+        print("%s\n" % (out_line), end=" ")
